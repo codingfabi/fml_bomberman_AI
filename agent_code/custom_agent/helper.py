17ea83d41438050f0eb16ed901d96b1a65392aa9
Fabian Kneissl <fkneissl@appointrix.de>
2021-03-12 14:52:50 +0100

    function to map actions to numeric, unsure if needed

--- agent_code/custom_agent/helper.py
+++ agent_code/custom_agent/helper.py
@@ -28,3 +28,12 @@ def state_to_features(game_state: dict) -> np.array:
     stacked_channels = np.stack(channels)
     # and return them as a vector
     return stacked_channels.reshape(-1)
+
+def action_to_numeric(actions: List[str] action):
+    """
+    this function maps an array string to a numeric list of actions 
+    lets see if we need this
+    """
+    numericList = np.zeros(len(actions))
+    actionIndex = actions.index(action)
+    numericList[actionIndex] = 1
